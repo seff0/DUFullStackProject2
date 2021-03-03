1c5a44be1d76f4d3da21cb0f1c39c6ba46d01b23
Greg Sonnier <gsonnier3@gmail.com>
2021-03-03 11:51:15 -0700

    changed model name to 'destination.js' for clarity

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: " YOUR PASSWORD HERE ",
+  password: "root",
   database: "destinations_db",
 });
 
