9246ac17f0d021c25452ec8ee69780ff4bcdb40c
Greg Sonnier <gsonnier3@gmail.com>
2021-03-04 11:28:26 -0700

    added basic user routes for login, signup, logout, current_trip, and fav_locs

---
+++ config/passport.js
@@ -0,0 +1,47 @@
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+
+var db = require("../models");
+
+//localstrategy refers to email/username based login. In this case, we want to use usernames
+passport.use(new LocalStrategy(
+    // Our user will sign in using an email, rather than a "username"
+    {
+      usernameField: "name"
+    },
+    function(name, password, done) {
+      // When a user tries to sign in this code runs
+        db.User.findOne({
+            where: {
+                name: name
+            }
+        }).then(function(dbUser) {
+            // If there's no user with the given name
+            if (!dbUser) {
+                return done(null, false, {
+                    message: "User does not exist."
+                });
+            }
+            // If there is a user with the given name, but the password the user gives us is incorrect
+            else if (!dbUser.validPassword(password)) {
+                return done(null, false, {
+                    message: "Incorrect password."
+                });
+            }
+            // If none of the above, return the user
+            return done(null, dbUser);
+        });
+    }
+));
+  
+//boilerplate to make passport work
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+  
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+  
+// Exporting our configured passport
+module.exports = passport;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,4 +2,61 @@ const db = require("../models");
 // Requiring our passport as we've configured it
 const passport = require("../config/passport");
 
-module.exports = (app) => {};
+module.exports = (app) => {
+    // If the user has valid login credentials, send them to the members page.
+    // Otherwise the user will be sent an error
+    app.post("/user/login", passport.authenticate("local"), function(req, res) {
+        res.json(req.user);
+    });
+    
+    //sign up a new user
+    app.post("/user/signup", function(req, res) {
+        db.User.create({
+            name: req.body.name,
+            password: req.body.password
+        })
+        .then(function() {
+            res.redirect(307, "/api/login");
+        })
+        .catch(function(err) {
+            res.status(401).json(err);
+        });
+    });
+    
+    
+    // Route for logging user out
+    app.get("/logout", function(req, res) {
+        req.logout();
+        res.redirect("/");
+    });
+    
+    //get the user's current trip
+    app.get("/user/current_trip", function(req, res) {
+        if (!req.user) {
+            // The user is not logged in, send back an empty object
+            res.json({});
+        } else {
+            // Otherwise send back the user's current trip
+            // trip should be an array of ids (each id corresponding to a location) separated by commas w/o spaces
+            // eg "1,3,5,6"
+            res.json({
+                current_trip: req.user.current_trip
+            });
+        }
+    });
+    
+    //get the user's favorite locations
+    app.get("/user/fav_locs", function(req, res) {
+        if (!req.user) {
+            // The user is not logged in, send back an empty object
+            res.json({});
+        } else {
+            // Otherwise send back the user's favorite locations
+            // fav_locs should be an array of ids (each id corresponding to a location) separated by commas w/o spaces
+            // eg "1,3,5,6"
+            res.json({
+                fav_locs: req.user.fav_locs
+            });
+        }
+    });
+};
