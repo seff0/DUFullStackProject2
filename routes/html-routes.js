f6f255741ba513d1332eb19ae6988bf0f8e6ffd4
seff0 <76668281+seff0@users.noreply.github.com>
2021-03-05 12:01:58 -0700

    set up handlebars rendering

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,31 +1,27 @@
-// Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+const express = require("express");
+const router = express.Router();
+const db = require("../models/");
 
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
-
-  app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/practiceindex.html"));
+router.get("/", (req, res) => {
+  db.Destination.findAll().then((data) => {
+    const dataObj = { destinations: data };
+    return res.render("index", dataObj);
   });
+});
 
-  app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/practiceindex.html"));
-  });
+// app.get("/login", function(req, res) {
+//   // If the user already has an account send them to the members page
+//   if (req.user) {
+//     res.redirect("/members");
+//   }
+//   res.sendFile(path.join(__dirname, "../public/practiceindex.html"));
+// });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/trip.html"));
-  });
-
-};
+// Here we've add our isAuthenticated middleware to this route.
+// If a user who is not logged in tries to access this route they will be redirected to the signup page
+// app.get("/members", isAuthenticated, function (req, res) {
+//   res.sendFile(path.join(__dirname, "../public/trip.html"));
+// });
+module.exports = router;
