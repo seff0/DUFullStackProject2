7a052325b5017b2717b04deace50d245125e7576
seff0 <76668281+seff0@users.noreply.github.com>
2021-03-08 16:39:20 -0700

    favorites handlers

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -15,11 +15,30 @@ module.exports = (app) => {
       email: req.body.email,
       password: req.body.password,
       current_trip: req.body.trip,
-      fav_locs: "1,2,5,6",
     })
       .then(function () {
         console.log("Logging in");
         res.redirect(307, "/api/login");
+      })
+      .catch(function (err) {
+        console.log("Something went wrong in user-routes!");
+        console.log(err);
+        console.log(req.body);
+        res.status(401).json(err);
+      });
+  });
+
+  app.post("/api/favs", (req, res) => {
+    let values = { fav_locs: req.body.favs };
+    let selector = {
+      where: {
+        email: req.user.email,
+      },
+    };
+    db.User.update(values, selector)
+      .then(function () {
+        console.log("Favorite locations updated.");
+        res.redirect("/end");
       })
       .catch(function (err) {
         console.log("Something went wrong in user-routes!");
